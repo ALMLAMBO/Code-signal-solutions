6a0368702a3dac8c610894b1fde7f623e6abffbb
Aleksander Marinov <36967653+ALMLAMBO@users.noreply.github.com>
2020-06-20 20:59:31 +0300

    Bug fix

--- CenturyFromYear/Program.cs
+++ CenturyFromYear/Program.cs
@@ -5,6 +5,16 @@ namespace CenturyFromYear {
 		static void Main(string[] args) {
 			int year = int.Parse(Console.ReadLine());
 
+			if(year % 100 == 0) {
+				Console.WriteLine(year / 100);
+				return;
+			}
+
+			if(year.ToString().Length <= 2) {
+				Console.WriteLine(1);
+				return;
+			}
+
 			string firstTwoDigits = year.ToString().Substring(0, 2);
 			int onlyFirstTwoDigits = int.Parse(firstTwoDigits);
 			Console.WriteLine(onlyFirstTwoDigits + 1);
